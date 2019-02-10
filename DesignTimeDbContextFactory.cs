146727057117fd54c3dcf67bafcc8300ffdd5b06
geeksarray <36873556+geeksarray@users.noreply.github.com>
2019-02-09 21:36:50 -0500

    Add files via upload

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace DataAccessLayer
+{
+   public class DesignTimeDbContextFactory : 
+        IDesignTimeDbContextFactory<StoreContext>
+   {
+        public StoreContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<StoreContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new StoreContext(builder.Options);
+        }
+    }
+ }
---
+++ StoreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        #region properties
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        #endregion
+    }
+}
