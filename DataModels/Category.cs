ac566e8c96d966085e0a58200b1129bdc9b8ada8
geeksarray <36873556+geeksarray@users.noreply.github.com>
2019-02-09 21:49:20 -0500

    Add files via upload

---
+++ DataModels/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccessLayer.DataModels
+{
+    public class Category
+    {
+        public Category()
+        {
+
+        }
+                
+        public int CategoryID { get; set; }
+                 
+        public string CategoryName { get; set; }
+                    
+        public string Description { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ DataModels/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccessLayer.DataModels
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+                
+        public int ProductID { get; set; }
+                        
+        public string ProductName { get; set; }
+                
+        public int CategoryID { get; set; }
+                
+        public int QuantityPerUnit { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int UnitsInStock { get; set; }
+                
+        public int ReorderLevel { get; set; }
+                
+        public bool Discontinued { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
